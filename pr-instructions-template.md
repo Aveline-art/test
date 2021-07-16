9d44e4d194a96e8f8940723e730ac5fbbe0f1198
avali4907 <avali4907@outlook.com>
2021-07-16 15:37:24 -0700

    made changes

---
+++ pr-instructions-template.md
@@ -0,0 +1,4 @@
+```
+git checkout -b ${nameOfCollaborator}-${nameOfFromBranch} ${nameOfIntoBranch}
+git pull https://github.com/${nameOfCollaborator}/website.git ${nameOfFromBranch}
+```
--- prInstructions.js
+++ prInstructions.js
@@ -1,12 +1,34 @@
+// Importing modules
+var fs = require("fs");
+
 // Global variables
 var github;
 var context;
 
 async function main({ g, c }) {
-  github = g;
-  context = c;
+    github = g;
+    context = c;
 
-  console.log(JSON.stringify(c))
+    postComment()
+}
+
+async function postComment() {
+    const body = createMessage()
+
+    const results = await github.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.payload.number,
+        body,
+    });
+
+    console.log(JSON.stringify(results));
+}
+
+function createMessage() {
+    const text = fs.readFileSync("./pr-instructions-template.md").toString('utf-8');
+    console.log(text);
+    
 }
 
 module.exports = main
